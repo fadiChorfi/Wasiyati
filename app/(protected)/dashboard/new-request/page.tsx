6d56ae8a47b315a5bc00b2e3ace460ccb1ce5ace
fadydodje <fadydodje@gmail.com>
2026-03-30 17:34:52 +0100

    feat: setting up the  pages of the user's dashboard

---
+++ app/(protected)/dashboard/new-request/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewRequestPage() {
+  return <div>NewRequestPage</div>;
+}
+
+export default NewRequestPage;
---
+++ app/(protected)/dashboard/wills/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function MyWillsPage() {
+  return <div>MyWillsPage</div>;
+}
+
+
+export default MyWillsPage;
